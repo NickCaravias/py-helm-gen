f7390963d4ed562cc5ab31e138e28f341c194e46
NickCaravias <nick.caravias@gmail.com>
2024-12-15 23:29:51 -0500

    finish up test cases

--- tests/test_helm_from_composer.py
+++ tests/test_helm_from_composer.py
@@ -28,61 +28,61 @@ class TestHelmFromComposer(unittest.TestCase):
             content = f.read()
             self.assertIn('apiVersion: v2', content)
             self.assertIn(f'name: {self.helm_generator.chart_name}', content)
-            self.assertIn(f'description: {self.helm_generator.desciption}', content)
+            self.assertIn(f'description: {self.helm_generator.description}', content)
             self.assertIn(f'version: {self.helm_generator.version}', content)
             self.assertIn(f'appVersion: {self.helm_generator.app_version}', content)
 
-    # def test_create_values_yaml(self):
-    #     self.helm_generator.create_values_yaml()
-    #     values_yaml_path = os.path.join(self.chart_dir, 'values.yaml')
-    #     self.assertTrue(os.path.exists(values_yaml_path))
+    def test_create_values_yaml(self):
+        self.helm_generator.create_values_yaml()
+        values_yaml_path = os.path.join(self.chart_dir, 'values.yaml')
+        self.assertTrue(os.path.exists(values_yaml_path))
 
-    #     with open(values_yaml_path, 'r') as f:
-    #         content = f.read()
-    #         self.assertIn('imagePullSecrets: []', content)
-    #         self.assertIn(f'replicaCount: {self.helm_generator.replicas}', content)
+        with open(values_yaml_path, 'r') as f:
+            content = f.read()
+            self.assertIn('imagePullSecrets: []', content)
+            self.assertIn(f'replicaCount: {self.helm_generator.replicas}', content)
 
-    # def test_generate_service(self):
-    #     service_name = 'web'
-    #     service_data = {
-    #         'image': 'nginx:latest',
-    #         'ports': ['80:80'],
-    #         'environment': {
-    #             'ENV_VAR': 'value'
-    #         }
-    #     }
-    #     self.helm_generator.generate_service(service_name, service_data)
-    #     service_yaml_path = os.path.join(self.chart_dir, 'templates', f'service-{service_name}.yaml')
-    #     self.assertTrue(os.path.exists(service_yaml_path))
+    def test_generate_service(self):
+        service_name = 'web'
+        service_data = {
+            'image': 'nginx:latest',
+            'ports': ['80:80'],
+            'environment': {
+                'ENV_VAR': 'value'
+            }
+        }
+        self.helm_generator.generate_service(service_name, service_data)
+        service_yaml_path = os.path.join(self.chart_dir, 'templates', f'service-{service_name}.yaml')
+        self.assertTrue(os.path.exists(service_yaml_path))
 
-    # def test_generate_deployment(self):
-    #     service_name = 'web'
-    #     service_data = {
-    #         'image': 'nginx:latest',
-    #         'ports': ['80:80'],
-    #         'environment': {
-    #             'ENV_VAR': 'value'
-    #         }
-    #     }
-    #     self.helm_generator._generate_deployment(service_name, service_data)
-    #     deployment_yaml_path = os.path.join(self.chart_dir, 'templates', f'deployment-{service_name}.yaml')
-    #     self.assertTrue(os.path.exists(deployment_yaml_path))
+    def test_generate_deployment(self):
+        service_name = 'web'
+        service_data = {
+            'image': 'nginx:latest',
+            'ports': ['80:80'],
+            'environment': {
+                'ENV_VAR': 'value'
+            }
+        }
+        self.helm_generator._generate_deployment(service_name, service_data)
+        deployment_yaml_path = os.path.join(self.chart_dir, 'templates', f'deployment-{service_name}.yaml')
+        self.assertTrue(os.path.exists(deployment_yaml_path))
 
-    # def test_add_values_for_service(self):
-    #     service_name = 'web'
-    #     service_data = {
-    #         'image': 'nginx:latest',
-    #         'ports': ['80:80'],
-    #         'environment': {
-    #             'ENV_VAR': 'value'
-    #         }
-    #     }
-    #     self.helm_generator._add_values_for_service(service_name, service_data)
-    #     self.assertIn(service_name, self.helm_generator.values_data)
-    #     self.assertEqual(self.helm_generator.values_data[service_name]['image']['repository'], 'nginx')
-    #     self.assertEqual(self.helm_generator.values_data[service_name]['image']['tag'], 'latest')
-    #     self.assertEqual(self.helm_generator.values_data[service_name]['env']['ENV_VAR'], 'value')
-    #     self.assertEqual(self.helm_generator.values_data[service_name]['ports'], ['80'])
+    def test_add_values_for_service(self):
+        service_name = 'web'
+        service_data = {
+            'image': 'nginx:latest',
+            'ports': ['80:80'],
+            'environment': {
+                'ENV_VAR': 'value'
+            }
+        }
+        self.helm_generator._add_values_for_service(service_name, service_data)
+        self.assertIn(service_name, self.helm_generator.values_data)
+        self.assertEqual(self.helm_generator.values_data[service_name]['image']['repository'], 'nginx')
+        self.assertEqual(self.helm_generator.values_data[service_name]['image']['tag'], 'latest')
+        self.assertEqual(self.helm_generator.values_data[service_name]['env']['ENV_VAR'], 'value')
+        self.assertEqual(self.helm_generator.values_data[service_name]['ports'], ['80'])
 
     def test_create_helm_chart(self):
         self.helm_generator.create_helm_chart()
@@ -91,8 +91,6 @@ class TestHelmFromComposer(unittest.TestCase):
         with open(values_yaml_path, 'r') as f:
             content = f.read()
         self.assertTrue(os.path.exists(os.path.join(self.chart_dir, 'Chart.yaml')))
-        self.assertTrue(os.path.exists(os.path.join(self.chart_dir, 'templates', 'deployment-web.yaml')))
-        self.assertTrue(os.path.exists(os.path.join(self.chart_dir, 'templates', 'service-web.yaml')))
 
 if __name__ == "__main__":
     unittest.main()
